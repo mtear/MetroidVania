afd581c2fc754329bd0449a615c02f9dedddf880
Nic Wilson <nic.edward.wilson@gmail.com>
2017-01-18 23:44:52 -0800

    Started Jump Command
    Added a Player Jump script with some simple jump functionality.

---
+++ MetroidVania/Assets/Scripts/PlayerJumpScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJumpScript : MonoBehaviour {
+
+    bool jumping = false, grounded = true;
+    int maxjumpingframes = 15, jumpingframes = 15;
+    float jumpspeed = 60;
+
+    Rigidbody rigidBody;
+
+    // Use this for initialization
+    void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (jumping)
+        {
+            jumpingframes--;
+            if (Input.GetKeyUp(KeyCode.A))
+            {
+                jumpingframes = 0;
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                if (jumpingframes > 0)
+                {
+                    rigidBody.velocity = rigidBody.velocity + Vector3.up * Time.deltaTime * jumpspeed * ((float)jumpingframes / (float)maxjumpingframes);
+                }
+                else
+                {
+                    jumping = false;
+                    jumpingframes = maxjumpingframes;
+                }
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.A) && grounded)
+            {
+                jumping = true;
+                grounded = false;
+            }
+        }
+    }
+}
---
+++ MetroidVania/Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.Translate( Vector3.left * Time.deltaTime * 3.0f);
+		}else if (Input.GetKey(KeyCode.RightArrow)) {
+			transform.Translate(Vector3.right * Time.deltaTime * 3.0f);
+		}
+
+	}
+}
